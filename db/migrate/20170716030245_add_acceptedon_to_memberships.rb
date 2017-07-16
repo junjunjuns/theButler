cc3ff5384ff58ba69343328faca0d11f4bc77cfe
Jun <x16123107@student.ncirl.ie>
2017-07-16 04:51:18 +0000

    updated group#home view

--- db/migrate/20170716030245_add_acceptedon_to_memberships.rb
+++ db/migrate/20170716030245_add_acceptedon_to_memberships.rb
@@ -1,5 +1,4 @@
 class AddAcceptedonToMemberships < ActiveRecord::Migration
   def change
-    add_column :memberships, :accepted_on, :timestamps
-  end
+    add_column :memberships, :accepted_on, :datetime
 end
